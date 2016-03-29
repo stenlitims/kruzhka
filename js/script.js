52c472d27aee83dfa8cc69009bf2979c9a10eae3
stenlitims <stenlitims@gmail.com>
2016-03-29 09:03:13 +0300

    main page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
-	if($('a[href="#"]').length > 0){
-		$(document).on('click', 'a[href="#"]',function(){
-			return false;
-		})
-	}
-    
+$(document).ready(function () {
+    if ($('a[href="#"]').length > 0) {
+        $(document).on('click', 'a[href="#"]', function () {
+            return false;
+        })
+    }
+
     $('form').validate();
-    
-	 
-		
+
+    $('.carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        nav: true,
+        navText: ['<i class="fa fa-caret-left"></i>', '<i class="fa fa-caret-right"></i>']
+    });
+    $('.list-sch').perfectScrollbar();
+
 });
