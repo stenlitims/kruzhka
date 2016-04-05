ee38341256d136b7fb2fcd609b1b9194480f1798
stenlitims <stenlitims@gmail.com>
2016-04-05 11:32:13 +0300

    Правки 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+function headerH() {
+    var h = $(window).height();
+    $('.home .header').css('height', h + 'px');
+}
+
+function getScrollBarWidth() {
+    var inner = document.createElement('p');
+    inner.style.width = "100%";
+    inner.style.height = "200px";
+    var outer = document.createElement('div');
+    outer.style.position = "absolute";
+    outer.style.top = "0px";
+    outer.style.left = "0px";
+    outer.style.visibility = "hidden";
+    outer.style.width = "200px";
+    outer.style.height = "150px";
+    outer.style.overflow = "hidden";
+    outer.appendChild(inner);
+    document.body.appendChild(outer);
+    var w1 = inner.offsetWidth;
+    outer.style.overflow = 'scroll';
+    var w2 = inner.offsetWidth;
+    if (w1 == w2) w2 = outer.clientWidth;
+    document.body.removeChild(outer);
+    return (w1 - w2);
+};
+
+
 $(document).ready(function () {
     if ($('a[href="#"]').length > 0) {
         $(document).on('click', 'a[href="#"]', function (e) {
@@ -16,21 +44,35 @@ $(document).ready(function () {
     });
     $('.list-sch').perfectScrollbar();
     $('.list-adress').perfectScrollbar();
-    
-    $(document).on('click', '.list-events a, .list-items a', function(e){
+
+    $(document).on('click', '.list-events a, .list-items a', function (e) {
         e.preventDefault();
-        $('html').addClass('o-hidden');
+        $('html').addClass('o-hidden').css('paddingRight', getScrollBarWidth() + 'px');
         $('.modal-art, .modal-mask').addClass('active');
     });
-    $(document).on('click', '.modal-art .close, .modal-mask', function(e){
+    $(document).on('click', '.modal-art .close, .modal-mask', function (e) {
         e.preventDefault();
-        $('html').removeClass('o-hidden');
+        $('html').removeClass('o-hidden').removeAttr('style');
         $('.modal-art, .modal-mask').removeClass('active');
     });
-    
-    $('.list-jobs .title span').on('click', function(){
+
+    $('.list-jobs .title span').on('click', function () {
         $(this).parent().next().slideToggle(200);
         $(this).toggleClass('active');
     });
 
+
+    if ($('.home').length > 0) {
+        headerH();
+        $(window).resize(function () {
+            headerH();
+        });
+    }
+    $('.navbar-toggle').on('click', function(){
+        $(this).toggleClass('active');
+    });
+
+
+
+    //console.log(getScrollBarWidth());
 });
